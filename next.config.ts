d3028bbfd6e85747c197fb5fed6375bb57bfa39c
daniel <daniel2mush@gmail.com>
2025-08-10 22:51:34 +0000

    added a change

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com", "lh3.googleusercontent.com"],
   },
+  experimental: {
+    esmExternals: "loose", // Allow ESM packages like @react-pdf/renderer
+  },
 };
 
 export default nextConfig;
